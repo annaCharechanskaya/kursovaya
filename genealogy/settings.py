61ad747e1f45ef9292010a6d9906a7d0c2f03e42
Charechanskaya Anna <cha.06@yandex.ru>
2025-12-20 03:54:12 +0300

    Update settings.py

--- genealogy/settings.py
+++ genealogy/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -120,6 +121,13 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Для WhiteNoise
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # genealogy/settings.py (в самый конец файла)
